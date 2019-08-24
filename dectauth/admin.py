1111983994e391522c39e372cb74c6a33446f155
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-08-24 15:56:13 +0200

    Add mark solved button to challenge admin

--- dectauth/admin.py
+++ dectauth/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.utils import timezone
+from django.urls import reverse
+from django.conf.urls import url
+from django.utils.html import format_html
 
 from asgiref.sync import async_to_sync
 
@@ -9,10 +13,24 @@ from .models import Challenge
 
 
 class ChallengeAdmin(admin.ModelAdmin):
-    list_display = ('username', 'key', 'created', 'last_seen', 'solved')
+    list_display = (
+        'username', 'key', 'created', 'last_seen', 'solved',
+        'mark_solved_button'
+    )
     list_filter = ('solved',)
     search_fields = ('username',)
     actions = ['solve_challenge']
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            url(
+                r'^(?P<obj_id>.+)/mark-solved/$',
+                self.admin_site.admin_view(self.mark_solved),
+                name='dectauth-challenge-mark_solved',
+            ),
+        ]
+        return custom_urls + urls
 
     def solve_challenge(self, request, queryset):
         channel_layer = get_channel_layer()
@@ -21,6 +39,23 @@ class ChallengeAdmin(admin.ModelAdmin):
             async_to_sync(channel_layer.group_send)(
                 str(challenge.uuid), {"type": "solved"}
             )
+
+    def mark_solved_button(self, obj):
+        if obj.solved:
+            return ''
+        return format_html(
+            '<button class="button" data-solvechallenge="{}">Mark solved</button>',
+            reverse('admin:dectauth-challenge-mark_solved', args=[obj.pk])
+        )
+    mark_solved_button.short_description = 'Mark solved'
+    mark_solved_button.allow_tags = True
+
+    def mark_solved(self, request, obj_id, *args, **kwargs):
+        if request.method != 'POST':
+            return
+        self.solve_challenge(request, Challenge.objects.filter(id=obj_id))
+        self.message_user(request, 'Success')
+        return HttpResponse()
 
 
 admin.site.register(Challenge, ChallengeAdmin)
